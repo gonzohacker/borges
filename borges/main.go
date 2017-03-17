60c907a5c0940526fb5cf8856c372a4a5e7c0867
Gonzo Hacker <gonzo@bitcap.com>
2017-03-17 14:38:42 -0500

    Create main.go
    A slave, a leper or a beggar could act as mystagogues. Also a child can indoctrinate another child. The act itself is trivial, momentary and requires no description. The materials are cork, wax or gum arabic. (The liturgy speaks of silt; this is also often used.) There are no temples specially dedicated to the celebration of this cult, but a ruin, a basement or hall are considered propitious sites.

---
+++ borges/main.go
@@ -0,0 +1,10 @@
+// By moving the application's package main logic into
+// a package other than main, it becomes much easier to
+// create infinite instances of recursive paradoxes 
+
+package main
+
+func main() {
+	// return random bytes that represent every possible implementation of 
+  // every possible program
+}
